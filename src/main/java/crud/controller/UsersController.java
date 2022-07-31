0d75d3371f806dba49cd2de3ea0fd2416b41a7e7
Work <79774488441@ya.ru>
2022-07-31 17:39:14 +0300

    process Edit

--- src/main/java/crud/controller/UsersController.java
+++ src/main/java/crud/controller/UsersController.java
@@ -5,15 +5,8 @@ import crud.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("/users")
@@ -25,14 +18,6 @@ public class UsersController {
     public UsersController(UserService userService) {
         this.userService = userService;
     }
-
-//    @GetMapping()
-//    public ModelAndView main() {
-//        List<User> listUser = userService.getAll();
-//        ModelAndView mav = new ModelAndView("users");
-//        mav.addObject("listUser", listUser);
-//        return mav;
-//    }
 
     @GetMapping()
     public String main(Model model) {
@@ -50,5 +35,11 @@ public class UsersController {
     public String createUser(@ModelAttribute("user") User user) {
         userService.save(user);
         return "redirect:/users";
+    }
+
+    @RequestMapping("/editUser")
+    public String editUser(@RequestParam long id, Model model) {
+        model.addAttribute("user", userService.get(id));
+        return "editUser";
     }
 }
