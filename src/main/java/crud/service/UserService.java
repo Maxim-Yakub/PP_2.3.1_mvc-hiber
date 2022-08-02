aff47d2021b730e7d6bd5f138c420706ac00a3da
Work <79774488441@ya.ru>
2022-08-02 12:21:20 +0300

    transaction and IoC

--- src/main/java/crud/service/UserService.java
+++ src/main/java/crud/service/UserService.java
@@ -1,49 +1,16 @@
 package crud.service;
 
 import crud.model.User;
-import crud.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
-@Service
+public interface UserService {
+     void save(User user);
+    List<User> getAll ();
+    User get(Long id);
+    void delete(Long id);
+    void update(Long id, User updatedUser);
+    List<User> search(String keyword);
 
-//перехват Spring Data JPA
-@Transactional
-public class UserService {
-    final
-    UserRepository repository;
 
-    @Autowired
-    public UserService(UserRepository repository) {
-        this.repository = repository;
-    }
-
-    public void save (User user) {
-        repository.save(user);
-    }
-
-    public List<User> getAll () {
-        return (List<User>) repository.findAll();
-    }
-
-    public User get(Long id) {
-        return repository.findById(id).get();
-    }
-
-    public void delete(Long id) {
-        repository.deleteById(id);
-    }
-
-    public void update(Long id, User updatedUser){
-        User userToBeUpdated = get(id);
-        userToBeUpdated.setName(updatedUser.getName());
-        userToBeUpdated.setLastName(updatedUser.getLastName());
-        userToBeUpdated.setEmail(updatedUser.getEmail());
-    }
-
-    public List<User> search(String keyword) {
-        return repository.search(keyword);
-    }
 }
